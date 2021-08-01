4d7f9b0b9a5b203c72c22018fce1f3012e8a4bde
Hemershon Silva <hemershon@gmail.com>
2021-08-01 19:10:58 -0300

    variaveis Ruby

---
+++ variaveis.rb
@@ -0,0 +1,22 @@
+# declaração locais, instância, global e uma constante
+# declara integer
+ age = 37 # variável local
+
+ # declara float
+
+ @num = 23.3 # variavel de instância
+
+#declara string
+
+nome = 'Hemershon'
+
+# declara boolean
+$teste = true # variavel 
+
+# declara nil 
+
+@teste2 = ""
+
+# declara array
+
+TESTE = [1, 2, 3, 4]
