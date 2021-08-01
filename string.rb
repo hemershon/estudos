bc6c6340b2985e8ff44924779a830fb9162853f8
Hemershon Silva <hemershon@gmail.com>
2021-08-01 19:33:59 -0300

    string  Ruby

---
+++ string.rb
@@ -0,0 +1,19 @@
+# declaração de uma string
+
+hello = "Olá"
+world = " Mundo!"
+
+#concatenar string
+
+puts hello + world 
+
+#interpolar 
+
+puts "#{hello} #{world}"
+
+#append usando 
+
+string = "Olá"
+string &lt;&lt; " Mundo!"
+
+puts string
